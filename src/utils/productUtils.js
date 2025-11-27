73aa6078187a35342efce897f0befadc704b49a9
Paula Frias est <pau.frias@duocuc.cl>
2025-11-27 18:26:28 -0300

    delete/feat: se elimina dataproductos y se deja productUtils donde tiene una funcion para convertir las variables a imgs

---
+++ src/utils/productUtils.js
@@ -0,0 +1,23 @@
+import manzana from "../assets/img/manzana.jpg";
+import naranja from "../assets/img/naranja.jpg";
+import platano from "../assets/img/platano.jpg";
+import zanahoria from "../assets/img/zanahoria.jpg";
+import espinaca from "../assets/img/espinaca.jpg";
+import pimenton from "../assets/img/pimenton.jpg";
+import miel from "../assets/img/miel.jpg";
+import quinoa from "../assets/img/quinoa.jpg";
+import leche from "../assets/img/leche.jpg";
+import defaultImage from "../assets/img/img-principal.jpg";
+
+export const productImages = {
+  "manzana.jpg": manzana,
+  "naranja.jpg": naranja,
+  "platano.jpg": platano,
+  "zanahoria.jpg": zanahoria,
+  "espinaca.jpg": espinaca,
+  "pimenton.jpg": pimenton,
+  "miel.jpg": miel,
+  "quinoa.jpg": quinoa,
+  "leche.jpg": leche,
+  default: defaultImage,
+};
